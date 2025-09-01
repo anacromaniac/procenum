69ade9e07d699be1d874e93e7718a6703a3702e6
Michele Biondi <michelebiondi01@gmail.com>
2025-09-01 19:51:31 +0200

    Add code to open process handle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use windows::Win32::Foundation::{CloseHandle, HANDLE};
 use windows::Win32::System::ProcessStatus::EnumProcesses;
+use windows::Win32::System::Threading::{OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ};
 
 fn main() {
     let mut lpidprocess: [u32; 1024] = [0; 1024];
@@ -6,17 +8,24 @@ fn main() {
     let mut lpcbneeded: u32 = 0;
 
     unsafe {
-        match EnumProcesses(lpidprocess.as_mut_ptr(), cb, &mut lpcbneeded) {
-            Ok(()) => {
-                let num_processes = lpcbneeded as usize / std::mem::size_of::<u32>();
+        EnumProcesses(lpidprocess.as_mut_ptr(), cb, &mut lpcbneeded).expect("EnumProcesses failed");
+        let num_processes: usize = lpcbneeded as usize / std::mem::size_of::<u32>();
+        println!("Number of processes detected: {}", num_processes);
 
-                for pid in lpidprocess.iter().take(num_processes) {
-                    println!("Process ID: {}", pid);
-                }
+        for &pid in &lpidprocess[..num_processes] {
+            if pid == 0 {
+                continue;
             }
-            Err(e) => {
-                eprintln!("EnumProcesses failed: {:?}", e);
-            }
+
+            let handle: HANDLE =
+                match OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, pid) {
+                    Ok(h) => h,
+                    Err(_e) => continue,
+                };
+
+            println!("Process ID: {}", pid);
+
+            let _ = CloseHandle(handle);
         }
     }
 }
